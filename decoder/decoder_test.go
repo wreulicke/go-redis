975b8f623b061f37fc727eb5f4c7519a7a5362d7
wreulicke <wreulicke@gmail.com>
2022-08-28 11:53:30 +0900

    Rewrite all to table tests

--- decoder/decoder_test.go
+++ decoder/decoder_test.go
@@ -9,36 +9,63 @@ import (
 )
 
 func TestDecodeSimpleString(t *testing.T) {
-	b := bytes.NewBufferString("+aaaa\r\n")
-	d := New(b)
+	tests := []struct {
+		input    string
+		expected data.Data
+	}{
+		{"+aaaa\r\n", &data.String{Value: "aaaa"}},
+		{"+string bbb\r\n", &data.String{Value: "string bbb"}},
+		{"+-12321421aaaa\r\n", &data.String{Value: "-12321421aaaa"}},
+	}
 
-	r, e := d.Decode()
-	assert.NoError(t, e)
-	result, ok := r.(*data.String)
-	assert.True(t, ok)
-	assert.Equal(t, "aaaa", result.Value)
+	for _, test := range tests {
+		b := bytes.NewBufferString(test.input)
+		d := New(b)
+
+		actual, err := d.Decode()
+		assert.NoError(t, err)
+		assertData(t, test.expected, actual)
+	}
 }
 
 func TestDecodeError(t *testing.T) {
-	b := bytes.NewBufferString("-unexpected\r\n")
-	d := New(b)
+	tests := []struct {
+		input    string
+		expected data.Data
+	}{
+		{"-unexpected\r\n", &data.Error{Message: "unexpected"}},
+		{"-:-1231985214*2193820194812tring bbb\r\n", &data.Error{Message: ":-1231985214*2193820194812tring bbb"}},
+	}
 
-	r, e := d.Decode()
-	assert.NoError(t, e)
-	result, ok := r.(*data.Error)
-	assert.True(t, ok)
-	assert.Equal(t, "unexpected", result.Message)
+	for _, test := range tests {
+		b := bytes.NewBufferString(test.input)
+		d := New(b)
+
+		actual, err := d.Decode()
+		assert.NoError(t, err)
+		assertData(t, test.expected, actual)
+	}
 }
 
 func TestDecodeInteger(t *testing.T) {
-	b := bytes.NewBufferString(":-12020205891308653\r\n")
-	d := New(b)
 
-	r, e := d.Decode()
-	assert.NoError(t, e)
-	result, ok := r.(*data.Integer)
-	assert.True(t, ok)
-	assert.Equal(t, int64(-12020205891308653), result.Value)
+	tests := []struct {
+		input    string
+		expected data.Data
+	}{
+		{":1232414\r\n", &data.Integer{Value: 1232414}},
+		{":-12020205891308653\r\n", &data.Integer{Value: -12020205891308653}},
+	}
+
+	for _, test := range tests {
+		b := bytes.NewBufferString(test.input)
+		d := New(b)
+
+		actual, err := d.Decode()
+		assert.NoError(t, err)
+		assertData(t, test.expected, actual)
+	}
+
 }
 
 func TestDecodeBulkString(t *testing.T) {
@@ -86,14 +113,4 @@ func TestDecodeArray(t *testing.T) {
 func assertData(t *testing.T, expected, actual data.Data) {
 	t.Helper()
 	assert.Equal(t, expected, actual)
-}
-
-func assertArray(t *testing.T, expected *data.Array, actual data.Data) {
-	t.Helper()
-	r, ok := actual.(*data.Array)
-	if !ok {
-		t.Error("actual is not array")
-		return
-	}
-	assert.Equal(t, expected.Values, r.Values)
 }
